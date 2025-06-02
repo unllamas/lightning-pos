ec606883db3c0666dcd3b85725d39a40b2893b37
Jona <llamasjonat@gmail.com>
2025-06-02 13:58:37 -0300

    fix(component): changed structure for LoadingSpinner

--- components/ui/loading-spinner.tsx
+++ components/ui/loading-spinner.tsx
@@ -1,9 +1,5 @@
 import { LoaderCircle } from 'lucide-react';
 
 export function LoadingSpinner() {
-  return (
-    <div className='animate-spin rounded-full h-8 w-8'>
-      <LoaderCircle className='h-8 w-8' />
-    </div>
-  );
+  return <LoaderCircle className='h-8 w-8 animate-spin' />;
 }
