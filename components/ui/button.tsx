8cfc27c4373d61bded036114658e8f737e23be7e
Jona <llamasjonat@gmail.com>
2025-06-20 21:19:40 -0300

    style(component): add new variant success on Button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -10,9 +10,12 @@ const buttonVariants = cva(
     variants: {
       variant: {
         default: 'bg-primary text-primary-foreground hover:bg-primary/90',
-        destructive: 'bg-destructive text-destructive-foreground hover:bg-destructive/90',
-        outline: 'border border-input bg-background hover:bg-accent hover:text-accent-foreground',
-        secondary: 'bg-secondary text-secondary-foreground hover:bg-secondary/80',
+        secondary: 'bg-white/10 text-white hover:bg-white/20',
+        destructive:
+          'bg-destructive text-destructive-foreground hover:bg-destructive/90 disabled:bg-destructive/10 disabled:text-destructive',
+        success: 'bg-green-600 text-white hover:bg-green-700 disabled:bg-green-600/10 disabled:text-green-400',
+        outline:
+          'border border-input bg-background hover:bg-accent hover:text-accent-foreground disabled:bg-background/10',
         ghost: 'hover:bg-accent hover:text-accent-foreground',
         link: 'text-primary underline-offset-4 hover:underline',
       },
