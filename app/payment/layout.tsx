f4506d0bb7f268701bfbf7850b41db00ece24c80
Jona <llamasjonat@gmail.com>
2025-07-05 03:54:04 -0300

    fix(layout): remove auth from Payment page

--- app/payment/layout.tsx
+++ app/payment/layout.tsx
@@ -1,10 +1,9 @@
 import type { Metadata } from 'next';
-import { AuthProvider } from '@/context/auth';
 
 export const metadata: Metadata = {
   title: 'Payment',
 };
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
-  return <AuthProvider>{children}</AuthProvider>;
+  return <>{children}</>;
 }
